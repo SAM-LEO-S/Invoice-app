ae0801a547f3b2bdc35e53fc59f7699f07cdd053
SAM-LEO-S <118737659+SAM-LEO-S@users.noreply.github.com>
2025-05-08 11:18:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -614,6 +614,8 @@ app.get('/download/:filename', (req, res) => {
     res.download(file);
 });
 
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
